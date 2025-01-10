56f18a61488bb79772239d33f53410df3fbaf44a
Jatin Madaan <jatinmadaan9882@gmail.com>
2025-01-11 03:16:37 +0530

    thymeleaf fragment

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,6 +3,8 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -13,6 +15,17 @@ public class PageController {
         model.addAttribute("name", "Smart Contact Manager");
         model.addAttribute("GitHubRepo", "https://github.com/Jatin-Madaan/SCM");
         return "home";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage(Model model, @RequestParam Boolean isLogin) {
+        model.addAttribute("isLogin", isLogin);
+        return "about";
+    }
+    
+    @GetMapping("/services")
+    public String servicesPage() {
+        return "services";
     }
     
 }
