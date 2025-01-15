81641dea6178808b81612baefa15f2b09bec0eec
Jatin Madaan <jatinmadaan9882@gmail.com>
2025-01-16 05:24:18 +0530

    responsive navbar

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,7 +3,6 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -25,6 +24,21 @@ public class PageController {
     @GetMapping("/services")
     public String servicesPage() {
         return "services";
+    }
+
+    @GetMapping("/contact")
+    public String contactPage() {
+        return "contact";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
     }
     
 }
