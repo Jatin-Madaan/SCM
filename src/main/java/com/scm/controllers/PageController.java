d1f2fb84781779257326c7820521a3a3a78a9979
Jatin Madaan <jatinmadaan9882@gmail.com>
2025-02-08 04:59:05 +0530

    Signup Page

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,8 +3,11 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-
+import com.scm.entities.User;
+import com.scm.forms.UserForm;
 
 @Controller
 public class PageController {
@@ -20,7 +23,7 @@ public class PageController {
     public String aboutPage() {
         return "about";
     }
-    
+
     @GetMapping("/services")
     public String servicesPage() {
         return "services";
@@ -37,8 +40,23 @@ public class PageController {
     }
 
     @GetMapping("/register")
-    public String registerPage() {
+    public String registerPage(Model model) {
+        UserForm userForm = new UserForm();
+        // default values bhi set kar sakte hain
+        // userForm.setName("Jatin Madaan");
+        model.addAttribute("userForm", userForm);
         return "register";
     }
-    
+
+    // processing register form
+    @PostMapping("/do-register")
+    public String processRegister(@ModelAttribute UserForm userForm) {
+        // fetching data from form
+        System.out.println("Register form submitted : " + userForm);
+        // validating user data
+        // saving data to database
+        // redirecting to login page
+        return "redirect:/register";
+    }
+
 }
---
+++ src/main/java/com/scm/entities/Contact.java
@@ -0,0 +1,41 @@
+package com.scm.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contact {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String picture;
+    private String description;
+    private boolean isFavorite = false;
+    private String websiteLink;
+    private String linkedInLink;
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany(mappedBy = "contact", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    private List<SocialLink> link = new ArrayList<>();
+}
